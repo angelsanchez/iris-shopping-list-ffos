907729a6179eef72c1b13ed42e7cdd0bd4df359b
angelsanchez <angel.sanchez@intelygenz.com>
2013-06-23 21:18:32 +0200

    Fix button add product style

--- app/ui/product/product_item.js
+++ app/ui/product/product_item.js
@@ -1,43 +1,43 @@
 iris.ui(function (self) {
 
-    self.settings({
-        productId : null
-    });
+	self.settings({
+		productId : null
+	});
 
-    var products = iris.resource(iris.path.products);
+	var products = iris.resource(iris.path.products);
 
-    self.create = function() {
-        
-        self.tmplMode(self.APPEND);
-        self.tmpl(iris.path.product_item.html);
+	self.create = function() {
+		
+		self.tmplMode(self.APPEND);
+		self.tmpl(iris.path.product_item.html);
 
-        self.inflate({
-            product : product()
-        });
+		self.inflate({
+			product : product()
+		});
 
-        self.get("switcher").on("change", switcherOnChange);
+		self.get("switcher").on("change", switcherOnChange);
 
-        render();
-    };
+		render();
+	};
 
-    // Get product data from the product resource
-    function product() {
-        return products.get( self.setting("productId") );
-    }
+	// Get product data from the product resource
+	function product() {
+		return products.get( self.setting("productId") );
+	}
 
-    // User events
-    function switcherOnChange () {
-        products.toggle( self.setting("productId") );
-        render();
-    }
+	// User events
+	function switcherOnChange () {
+		products.toggle( self.setting("productId") );
+		render();
+	}
 
-    // Update screen interface using the product resource
-    function render () {
-        if ( product().checked ) {
-            self.get().addClass("product-checked");
-        } else {
-            self.get().removeClass("product-checked");
-        }
-    }
+	// Update screen interface using the product resource
+	function render () {
+		if ( product().checked ) {
+			self.get().addClass("product-checked");
+		} else {
+			self.get().removeClass("product-checked");
+		}
+	}
 
 },iris.path.product_item.js);
